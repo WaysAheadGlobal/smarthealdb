77546245e67c6ce76fa5b85c162d74af052f77a3
Vishnukv2 <160594832+Vishnukv2@users.noreply.github.com>
2024-06-04 21:25:39 +0530

    Update woundapi.py

--- woundapi.py
+++ woundapi.py
@@ -514,7 +514,7 @@ def send_otp():
                 update_otp_in_database(session, phone, otp, expiry_time)
                 
                 token = jwt.encode({'email': organisation.email, 'exp': datetime.datetime.utcnow() + datetime.timedelta(days=30)}, JWT_SECRET_KEY, algorithm='HS256')
-                return jsonify({'status': 200, 'message': 'OTP Sent on mobile.', 'token': token, 'otp': otp}), 200
+                return jsonify({'status': 200, 'message': 'OTP Sent on mobile.', 'token': token, 'otp': otp, 'email': organisation.email}), 200
             else:
                 return jsonify({'status': 0, 'message': 'OOPS! Phone Does Not Exist!'}), 404
     except Exception as e:
