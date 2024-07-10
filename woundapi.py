d63f7b8b11f6fcb8e9c63b7b33e8fe85f37cfef2
Vishnukv2 <160594832+Vishnukv2@users.noreply.github.com>
2024-07-10 12:11:52 +0530

    Update woundapi.py

--- woundapi.py
+++ woundapi.py
@@ -528,7 +528,7 @@ def send_otp():
                 update_otp_in_database(session, phone, otp, expiry_time)
                 
                 token = jwt.encode({'email': organisation.email, 'exp': datetime.datetime.utcnow() + datetime.timedelta(days=30)}, JWT_SECRET_KEY, algorithm='HS256')
-                return jsonify({'status': 200, 'message': 'OTP Sent on mobile.', 'token': token, 'otp': otp, 'email': organisation.email, 'name': organisation.name}), 200
+                return jsonify({'status': 200, 'message': 'OTP Sent on mobile.', 'token': token, 'otp': otp, 'email': organisation.email, 'name': organisation.name, 'pin': organisation.pin}), 200
             else:
                 return jsonify({'status': 0, 'message': 'OOPS! Phone Does Not Exist!'}), 404
     except Exception as e:
@@ -791,7 +791,7 @@ def med_send_otp():
                 update_otp_in_database(session, phone, otp, expiry_time)
                 
                 token = jwt.encode({'email': user.email, 'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=24)}, JWT_SECRET_KEY, algorithm='HS256')
-                return jsonify({'status': 200, 'message': 'OTP Sent on mobile.', 'token': token, 'otp': otp, 'email': user.email, 'name': user.name}), 200
+                return jsonify({'status': 200, 'message': 'OTP Sent on mobile.', 'token': token, 'otp': otp, 'email': user.email, 'name': user.name, 'pin': user.pin}), 200
             else:
                 return jsonify({'status': 0, 'message': 'OOPS! Phone Does Not Exist!'}), 404
     except Exception as e:
